a8940df29a50df356db609555d1e10f32d1b3b69
amzxyz <rcm@qq.com>
2025-05-11 07:57:01 +0800

    更新

--- lua/tone_fallback.lua
+++ lua/tone_fallback.lua
@@ -5,7 +5,8 @@ local function tone_fallback(input, env)
     local ctx = env.engine.context
     -- 获取当前输入的内容
     local input_text = ctx.input
-    local is_last_digit = input_text:match("%d%d") -- 检查最后两个字符是否为数字
+    -- 检查最后两个字符是否为数字
+    local is_last_digit = input_text:match("%d%d")
     -- 如果是连续的两个数字
     if is_last_digit then
         local digit_count = 0
@@ -19,12 +20,14 @@ local function tone_fallback(input, env)
         end
         -- 如果有两个连续数字，进行回退并插入新的数字
         if digit_count == 2 then
-            -- 删除最后一个数字
+            -- 删除最后两个数字
             ctx:pop_input(2)
             -- 获取当前按下的数字并插入
             local last_char = input_text:sub(-1)
             ctx:push_input(last_char)
+            return 1
         end
     end
+    return 2
 end
 return tone_fallback
