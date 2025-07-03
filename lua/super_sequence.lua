2b143e05b0c5adf46f1d83f33392a79d9ff2d605
everyx <lunt.luo@gmail.com>
2025-07-03 21:36:59 +0800

    fix(lua): super_sequence 应使用光标签的作为输入码

--- lua/super_sequence.lua
+++ lua/super_sequence.lua
@@ -87,6 +87,12 @@ local function getUserSegment(input)
     return table
 end
 
+---@param context Context
+---@return string
+local function get_valid_input(context)
+    return context.input:sub(context.caret_pos)
+end
+
 local P = {}
 function P.init() end
 
@@ -129,8 +135,9 @@ function P.func(key_event, env)
     end
 
     if cur_offset == nil then -- 如果是重置/置顶，直接设置位置
-        saveUserSegment(context.input, selected_cand.text, is_pin and 1 or nil)
-    else                      -- 否则进入 filter 调整位移
+        local valid_input = get_valid_input(context)
+        saveUserSegment(valid_input, selected_cand.text, is_pin and 1 or nil)
+    else -- 否则进入 filter 调整位移
         cur_selected_text = selected_cand.text
     end
 
@@ -155,7 +162,8 @@ end
 ---@param env Env
 function F.func(input, env)
     local context = env.engine.context
-    local user_segment = getUserSegment(context.input)
+    local valid_input = get_valid_input(context)
+    local user_segment = getUserSegment(valid_input)
     local need_reorder = user_segment ~= nil or (cur_selected_text ~= nil and cur_offset ~= 0 and cur_offset ~= nil)
 
     if not need_reorder then -- 如果没有自定义排序，不用去重，直接 yield 并返回
@@ -240,7 +248,7 @@ function F.func(input, env)
 
                 table.remove(reordered_candidates, from_position)
                 table.insert(reordered_candidates, to_position, cur_selected_cand)
-                saveUserSegment(context.input, cur_selected_text, to_position)
+                saveUserSegment(valid_input, cur_selected_text, to_position)
                 cur_highlight_idx = to_position - 1
             end
         end
